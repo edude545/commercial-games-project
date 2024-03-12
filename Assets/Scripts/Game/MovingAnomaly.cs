dac893e6bb6be8de997ca622f41093bd1b5fc56b
lyradodds <92803162+lyradodds@users.noreply.github.com>
2024-03-12 01:01:59 +0000

    commit

---
+++ Assets/Scripts/Game/MovingAnomaly.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingAnomaly : Anomaly {
+
+    private Vector3 initialPosition;
+    public Transform destinationPosition;
+
+    public void Awake() {
+        initialPosition = transform.position;
+    }
+
+    public override void OnAnomalyTriggered() {
+        transform.position = destinationPosition.position;
+        Debug.Log("Object shifted!");
+    }
+
+}
