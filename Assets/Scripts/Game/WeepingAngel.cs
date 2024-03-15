73b065d4a4b3b7db3cee47dcbda6b6cafb8875ef
Lfriskney <luke.friskney21@bathspa.ac.uk>
2024-03-15 19:00:02 +0000

    reference for code

--- Assets/Scripts/Game/WeepingAngel.cs
+++ Assets/Scripts/Game/WeepingAngel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+// code from www.youtube.com/watch?v=_e57zSZSOS8
 
 public class WeepingAngel : MonoBehaviour
 {
