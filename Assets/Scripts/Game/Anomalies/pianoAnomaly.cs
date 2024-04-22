3f3370f8c446b28e6eb4104281566ddb6c9d1346
Lfriskney <luke.friskney21@bathspa.ac.uk>
2024-04-22 14:07:33 +0100

    Addition of more prefabs

---
+++ Assets/Scripts/Game/Anomalies/pianoAnomaly.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pianoAnomaly : Anomaly
+{
+    private void Start()
+    {
+        setSoundActive(false);
+    }
+
+    public override void OnAnomalyFixed()
+    {
+        setSoundActive(false);
+    }
+
+    public override void OnAnomalyTriggered()
+    {
+        
+        setSoundActive(true);
+    }
+
+    private void setSoundActive(bool active)
+    {
+        GetComponent<AudioSource>().enabled = active;
+    }
+ 
+
+}
