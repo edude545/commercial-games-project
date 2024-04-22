4bdf87e2d9cdbc937d79d1080ed8102a9d6565e7
Lfriskney <luke.friskney21@bathspa.ac.uk>
2024-04-22 18:37:11 +0100

    Additions to rooms

--- Assets/Scripts/Game/Anomalies/MovingAnomaly.cs
+++ Assets/Scripts/Game/Anomalies/MovingAnomaly.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingAnomaly : Anomaly {
-
+public class MovingAnomaly : Anomaly
+{
     private Vector3 initialPosition;
+    private Quaternion initialRotation;
     public Transform destinationPosition;
+    public Transform destinationRotation;
 
-    public void Awake() {
+    public void Awake()
+    {
         initialPosition = transform.position;
+        initialRotation = transform.rotation;
     }
 
-    public override void OnAnomalyTriggered() {
+    public override void OnAnomalyTriggered()
+    {
         transform.position = destinationPosition.position;
+        transform.rotation = destinationRotation.rotation;
         Debug.Log("Object shifted!");
     }
 
-    public override void OnAnomalyFixed() {
+    public override void OnAnomalyFixed()
+    {
         transform.position = initialPosition;
+        transform.rotation = initialRotation;
     }
-
 }
--- Assets/Scripts/Game/AreaUnlocks.cs
+++ Assets/Scripts/Game/AreaUnlocks.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AreaUnlocks : MonoBehaviour
 {
     private GameObject[] objectsWithTag;
     public int requiredAnomalyCount = 5; // Specify the required anomaly count
+    public TextMeshProUGUI messageText; // Reference to the UI Text element
+    public float messageDuration = 2f; // Duration for which the message will be displayed
+
+    private bool displayMessage = false;
+    private float messageTimer = 0f;
 
     void Start()
     {
         objectsWithTag = GameObject.FindGameObjectsWithTag("block");
         UpdateAreaUnlockStatus();
+        messageText.text = "Anomalies solved: " + Anomaly.anomalyCount;
     }
 
     void Update()
     {
         UpdateAreaUnlockStatus(); // Continuously update area unlock status
+
+        if (displayMessage)
+        {
+            messageTimer += Time.deltaTime;
+            if (messageTimer >= messageDuration)
+            {
+                // Hide the message after the duration
+                messageText.text = "";
+                displayMessage = false;
+                messageTimer = 0f;
+            }
+        }
     }
 
     void UpdateAreaUnlockStatus()
@@ -26,6 +45,7 @@ public class AreaUnlocks : MonoBehaviour
         }
         else
         {
+
             SetObjectsActive(true);
         }
     }
@@ -37,4 +57,17 @@ public class AreaUnlocks : MonoBehaviour
             obj.SetActive(active);
         }
     }
+
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("block"))
+        {
+            // Display a message when player collides with the collider
+            messageText.text = "Solve 10 anomalies, anomalies currently solved: " + Anomaly.anomalyCount;
+            displayMessage = true;
+        }
+    }
+
+
 }
